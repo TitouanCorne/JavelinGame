72efe6fef3a3a36451f2a58a7144d77fcf7d6ca3
empereurmodeste <95174330+empereurmodeste@users.noreply.github.com>
2022-11-20 16:18:13 +0100

    Update lancer.js

--- back/lancer.js
+++ back/lancer.js
@@ -17,3 +17,34 @@ function lancer_ManyDices(n){
 module.exports = {
     res_dices: lancer_ManyDices,
 }
+
+function lancerde() {
+    return Math.floor(Math.random() * 6 + 1); 
+  }
+
+tableau = [[lancerde(), false], [lancerde(), false], [lancerde(), false], [lancerde(), false], [lancerde(), false], [lancerde(), false]]
+
+function gelerunde(tableau, index){
+    if (tableau[index-1][0] % 2 == 0) {
+    	tableau[index-1][1] = true;
+    }
+    return tableau
+}
+
+function relancer(tableau) {
+    for(var i = 0; i < nb_des; i++){
+        if(tableau[i][1] = false){
+            tableau[i][0] = lancer();
+        }
+    }
+    return tableau
+}
+
+function somme(tableau){
+    S = 0;
+    for(var i = 0; i < L.length; i++){
+        S = S + L[i][0];
+    }
+    return S
+}
+
